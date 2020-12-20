3b4201547dc8bcd720f8e5d48952a5154320344f
Fidel C <coronel08@aol.com>
2020-12-20 00:12:28 -0800

    creating pages and navbar

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GridWrapper = styled.div `
+    display: grid;
+    grid-gap: 10px;
+    margin-top: 1em;
+    margin-left: 6em;
+    margin-right: 6em;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: minimax(25px, auto);
+`;
+
+export const About = () => (
+    <GridWrapper>
+        <h2>About Page</h2>
+        <p>testing the about page fill out later</p>
+        <p>testing still</p>
+    </GridWrapper>
+)
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GridWrapper = styled.div `
+    display:grid
+    grid-gap: 10px;
+    margin-top:1em;
+    margin-left:6em;
+    margin-right: 6em;
+    grid-template-columns: repeat(12, 1fr);
+    grid-auto-rows: minimax(25px, auto);
+`;
+
+export const Home = (props) => (
+    <GridWrapper>
+        <p>This is a paragraph for home page</p>
+        <p>Another paragraph </p>
+    </GridWrapper>
+)
